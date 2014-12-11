bf62d293e94c34863f052fa26e134323c363b8b9
Bennett Hiles <bennetthiles@gmail.com>
2014-12-11 14:02:28 -0800

    Updated to version 4.2.7

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #   class { 'libreoffice':
 #     version => '4.2.7',
 #   }
-class libreoffice($version='4.2.y') {
+class libreoffice($version='4.2.7') {
   package { "LibreOffice-${version}":
     provider => 'appdmg',
     source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86/LibreOffice_${version}_MacOS_x86.dmg",
